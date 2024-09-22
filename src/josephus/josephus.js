82dd98fae5e8f8f12bd80c2f23d58638b26582a5
Thomas Sheehan <tsheehan@indeed.com>
2024-09-22 17:31:11 +0900

    Provide solutions to LL questions

--- src/josephus/josephus.js
+++ src/josephus/josephus.js
@@ -1,3 +1,22 @@
-function josephus(list, m) {}
+const LinkedList = require("../lib/linkedList");
+
+function josephus(list, m) {
+  let node = list.head;
+  let tail = list.find((node) => !node.next);
+  tail.next = list.head;
+
+  while (node.next != node) {
+    for (let i = 1; i < m; i++) {
+      node = node.next;
+    }
+    if (node.next === list.head) {
+      list.head = list.head.next;
+    }
+
+    node.next = node.next.next;
+    node = node.next;
+  }
+  return node.value;
+}
 
 module.exports = josephus;
--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,38 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function swap(list, x, y) {
+  if (x === y || !list.head) {
+    return list; // No need to swap if they're the same or if the list is empty
+  }
+
+  let x_prev = list.findWithPrevious((node) => node === x)[1];
+  let y_prev = list.findWithPrevious((node) => node === y)[1];
+
+  // If x or y are not found in the list, return the list unchanged
+  if (!x || !y) {
+    return list;
+  }
+
+  // Swap the previous node's `next` pointers
+  if (x_prev) {
+    x_prev.next = y;
+  } else {
+    list.head = y; // If x is the head
+  }
+
+  if (y_prev) {
+    y_prev.next = x;
+  } else {
+    list.head = x; // If y is the head
+  }
+
+  // Now, swap the `next` pointers of x and y
+  let temp = x.next;
+  x.next = y.next;
+  y.next = temp;
+
+  return list;
+}
 
 module.exports = swap;
